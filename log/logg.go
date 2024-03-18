be6b443dc38bd610371c9cbb9f74b6ab2cf87001
INexizI <39536903+INexizI@users.noreply.github.com>
2024-03-18 16:54:16 +0300

    upd Log

---
+++ log/logg.go
@@ -0,0 +1,35 @@
+package logg
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	SystemLogger  *log.Logger
+	CommandLogger *log.Logger // TODO(D): create log for only bot commands
+	MessageLogger *log.Logger
+)
+
+func init() {
+	sysFile, err := os.OpenFile("log/system.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(sysFile)
+	SystemLogger = log.New(sysFile, "SYS: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	cmdFile, err := os.OpenFile("log/command.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(cmdFile)
+	CommandLogger = log.New(cmdFile, "CMD: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	msgFile, err := os.OpenFile("log/message.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(msgFile)
+	MessageLogger = log.New(msgFile, "MSG: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
