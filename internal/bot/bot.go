af3aa517d0253fdf38e390e6008b2a0109f9a3f3
INexizI <yavseh@hotmail.com>
2024-10-30 12:46:28 +0300

    add DM functional

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	logg "go-spooty/internal/log"
 	spotify "go-spooty/internal/spotify"
-	"log"
 	"math/rand"
 	"os"
 	"os/signal"
@@ -13,6 +12,12 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 )
+
+type Direct struct {
+	OriginChannelId string
+	Name            string
+	Login           string
+}
 
 type Quote struct {
 	Text  string
@@ -26,7 +31,69 @@ var (
 	BotGuild string
 	BotToken string
 	song     spotify.Song
+	DM       map[string]Direct = map[string]Direct{}
 )
+
+func (d *Direct) ToDirectMessageUser() discordgo.MessageEmbed {
+	return discordgo.MessageEmbed{
+		Title: "Your Name & Login:",
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:  "Name",
+				Value: d.Name,
+			},
+			{
+				Name:  "Login",
+				Value: d.Login,
+			},
+		},
+	}
+}
+
+func (d *Direct) ToDirectMessageGuild() discordgo.MessageEmbed {
+	return discordgo.MessageEmbed{
+		Title: "New User:",
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:  "Name",
+				Value: d.Name,
+			},
+		},
+	}
+}
+
+func Messages(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	} else {
+		logg.MessageLogger.Printf("%s: %s", m.Author.Username, m.Content)
+	}
+
+	if m.Content == "qwe" {
+		DirectMessages(s, m)
+	}
+
+	if m.GuildID == "" {
+		answers, ok := DM[m.ChannelID]
+		if !ok {
+			return
+		}
+
+		if answers.Name == "" {
+			answers.Name = m.Content
+			s.ChannelMessageSend(m.ChannelID, "Login?")
+			DM[m.ChannelID] = answers
+			return
+		} else {
+			answers.Login = m.Content
+			embedUser := answers.ToDirectMessageUser()
+			embedGuild := answers.ToDirectMessageGuild()
+			s.ChannelMessageSendEmbed(m.ChannelID, &embedUser)
+			s.ChannelMessageSendEmbed(answers.OriginChannelId, &embedGuild)
+			delete(DM, m.ChannelID)
+		}
+	}
+}
 
 func SongArtists() string {
 	json.Unmarshal([]byte(spotify.GetPlaybackState()), &song)
@@ -41,55 +108,6 @@ func SongArtists() string {
 	}
 
 	return artist
-}
-
-func Run() {
-	session, err := discordgo.New("Bot " + BotToken)
-	if err != nil {
-		logg.SystemLogger.Fatal(err)
-	}
-
-	_, err = session.ApplicationCommandBulkOverwrite(BotApp, BotGuild, []*discordgo.ApplicationCommand{
-		{
-			Name:        "song",
-			Description: "Get current Spotify song",
-		},
-		{
-			Name:        "quote",
-			Description: "Get random quote",
-		},
-		{
-			Name:        "first",
-			Description: "Showcase of a first slash command",
-		},
-		{
-			Name:        "second",
-			Description: "Showcase of a second slash command",
-		},
-	})
-	if err != nil {
-		logg.SystemLogger.Println(err)
-	}
-
-	session.AddHandler(Messages)
-	session.AddHandler(Commands)
-	// session.AddHandler(Roles)
-	session.Open()
-	defer session.Close()
-
-	fmt.Println("Bot running...")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-	logg.SystemLogger.Println("Bot shutdown")
-}
-
-func Messages(sess *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == sess.State.User.ID {
-		return
-	} else {
-		logg.MessageLogger.Printf("%s: %s", m.Author.Username, m.Content)
-	}
 }
 
 func EmbedMessageSpotify() []*discordgo.MessageEmbed {
@@ -133,11 +151,11 @@ func EmbedMessageSpotify() []*discordgo.MessageEmbed {
 	}
 }
 
-func Commands(sess *discordgo.Session, i *discordgo.InteractionCreate) {
+func Commands(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	data := i.ApplicationCommandData()
 	switch data.Name {
 	case "song":
-		err := sess.InteractionRespond(
+		err := s.InteractionRespond(
 			i.Interaction,
 			&discordgo.InteractionResponse{
 				Type: 4, // discordgo.InteractionResponseChannelMessageWithSource = 4
@@ -146,25 +164,25 @@ func Commands(sess *discordgo.Session, i *discordgo.InteractionCreate) {
 				},
 			},
 		)
+		logg.CommandLogger.Println("song command")
 		if err != nil {
 			logg.SystemLogger.Println(err)
 		}
 	case "quote":
 		file, err := os.ReadFile("internal/bot/quotes.json")
 		if err != nil {
-			log.Fatal("Error when opening file: ", err)
+			logg.SystemLogger.Fatalln("Error when opening file: ", err)
 		}
 
 		var data []Quote
 		err = json.Unmarshal(file, &data)
 		if err != nil {
-			log.Fatal("Error during Unmarshal(): ", err)
+			logg.SystemLogger.Fatalln("Error during Unmarshal(): ", err)
 		}
 
 		selection := rand.Intn(len(data))
-		log.Printf("%v", data[selection])
 
-		err = sess.InteractionRespond(
+		err = s.InteractionRespond(
 			i.Interaction,
 			&discordgo.InteractionResponse{
 				Type: 4, // discordgo.InteractionResponseChannelMessageWithSource = 4
@@ -179,12 +197,12 @@ func Commands(sess *discordgo.Session, i *discordgo.InteractionCreate) {
 				},
 			},
 		)
-		logg.CommandLogger.Printf("quote command")
+		logg.CommandLogger.Println("quote command")
 		if err != nil {
-			logg.SystemLogger.Println(err)
+			logg.SystemLogger.Panic(err)
 		}
 	case "first":
-		err := sess.InteractionRespond(
+		err := s.InteractionRespond(
 			i.Interaction,
 			&discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -194,12 +212,12 @@ func Commands(sess *discordgo.Session, i *discordgo.InteractionCreate) {
 				},
 			},
 		)
-		logg.CommandLogger.Printf("first command")
+		logg.CommandLogger.Println("first command")
 		if err != nil {
-			logg.SystemLogger.Println(err)
+			logg.SystemLogger.Panic(err)
 		}
 	case "second":
-		err := sess.InteractionRespond(
+		err := s.InteractionRespond(
 			i.Interaction,
 			&discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -208,19 +226,19 @@ func Commands(sess *discordgo.Session, i *discordgo.InteractionCreate) {
 				},
 			},
 		)
-		logg.CommandLogger.Printf("second command")
+		logg.CommandLogger.Println("second command")
 		if err != nil {
-			logg.SystemLogger.Println(err)
+			logg.SystemLogger.Panic(err)
 		}
 	default:
-		err := sess.InteractionRespond(
+		err := s.InteractionRespond(
 			i.Interaction,
 			&discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseType(i.ApplicationCommandData().Options[0].IntValue()),
 			},
 		)
 		if err != nil {
-			sess.FollowupMessageCreate(
+			s.FollowupMessageCreate(
 				i.Interaction,
 				true,
 				&discordgo.WebhookParams{
@@ -232,12 +250,72 @@ func Commands(sess *discordgo.Session, i *discordgo.InteractionCreate) {
 	}
 }
 
-// func Roles(sess *discordgo.Session, ra *discordgo.MessageReactionAdd, rr *discordgo.MessageReactionRemove) {
+// func Roles(s *discordgo.Session, ra *discordgo.MessageReactionAdd, rr *discordgo.MessageReactionRemove) {
 // 	if ra.Emoji.Name == "" {
-// 		sess.GuildMemberRoleAdd(ra.GuildID, ra.UserID, "roleID")
-// 		sess.ChannelMessageSend(ra.ChannelID, fmt.Sprintf("%v has been added to %v", ra.UserID, ra.Emoji.Name))
+// 		s.GuildMemberRoleAdd(ra.GuildID, ra.UserID, "roleID")
+// 		s.ChannelMessageSend(ra.ChannelID, fmt.Sprintf("%v has been added to %v", ra.UserID, ra.Emoji.Name))
 
-// 		sess.GuildMemberRoleRemove(rr.GuildID, rr.UserID, "roleID")
-// 		sess.ChannelMessageSend(rr.ChannelID, fmt.Sprintf("%v has been removed to %v", rr.UserID, rr.Emoji.Name))
+// 		s.GuildMemberRoleRemove(rr.GuildID, rr.UserID, "roleID")
+// 		s.ChannelMessageSend(rr.ChannelID, fmt.Sprintf("%v has been removed to %v", rr.UserID, rr.Emoji.Name))
 // 	}
 // }
+
+func DirectMessages(s *discordgo.Session, m *discordgo.MessageCreate) {
+	channel, err := s.UserChannelCreate(m.Author.ID)
+	if err != nil {
+		logg.SystemLogger.Panic(err)
+	}
+
+	if _, ok := DM[channel.ID]; !ok {
+		DM[channel.ID] = Direct{
+			OriginChannelId: m.ChannelID,
+			Name:            "",
+			Login:           "",
+		}
+		s.ChannelMessageSend(channel.ID, "Hello from Spooty Bot!")
+		s.ChannelMessageSend(channel.ID, "Please enter your name")
+	} else {
+		s.ChannelMessageSend(channel.ID, "Uh... Hello... 🧐")
+	}
+}
+
+func Run() {
+	session, err := discordgo.New("Bot " + BotToken)
+	if err != nil {
+		logg.SystemLogger.Fatal(err)
+	}
+
+	_, err = session.ApplicationCommandBulkOverwrite(BotApp, BotGuild, []*discordgo.ApplicationCommand{
+		{
+			Name:        "song",
+			Description: "Get current Spotify song",
+		},
+		{
+			Name:        "quote",
+			Description: "Get random quote",
+		},
+		{
+			Name:        "first",
+			Description: "Showcase of a first slash command",
+		},
+		{
+			Name:        "second",
+			Description: "Showcase of a second slash command",
+		},
+	})
+	if err != nil {
+		logg.SystemLogger.Println(err)
+	}
+
+	session.AddHandler(Messages)
+	session.AddHandler(Commands)
+	// session.AddHandler(Roles)
+	session.Open()
+	defer session.Close()
+
+	fmt.Println("Bot running...")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+	logg.SystemLogger.Println("Bot shutdown")
+}
